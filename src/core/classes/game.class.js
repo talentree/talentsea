9b5829c168096b5ce9e75a910aca1be3d191f844
Davide Peron <davide.peron19@gmail.com>
2019-08-13 18:36:34 +0200

    fine inserimento classi

---
+++ src/core/classes/game.class.js
@@ -0,0 +1,10 @@
+import { Info } from "./info.class";
+import { Team } from './team.class';
+
+export class Game {
+
+    constructor() {
+        this.info = new Info();
+        this.teams = {};
+    }
+}
