00e4e3f3cf8b76812ed8f5721f73a0c2a314a82c
Unknown <davide.peron19@gmail.com>
2019-09-03 10:48:48 +0200

    aggiunto titolo admin console

--- src/pages/admin-console/admin-console.page.js
+++ src/pages/admin-console/admin-console.page.js
@@ -1,5 +1,6 @@
 import { html } from '@polymer/lit-element';
 import { NavElement } from '../../core/nav-element';
+import { AdminState } from '../../core/states/admin.state';
 
 import './admin-new-game.component';
 
@@ -10,13 +11,25 @@ export class AdminConsolePage extends NavElement {
 
     render() {
         return html`
-            <h1 class="title">Admin console</h1>
+            <div class = " columns is-mobile is-centered is-full ">
+                <div class = " column is-11 ">
+                    <h1  class = " title is-size-1 has-text-centered has-text-primary is-italic has-text-weight-bold gradient-text ">AMMINISTRATORE</h1>
+                    <div class = "home-position" >
+                        <a route="/"><i class="fas fa-home icon is-medium"></i></a>
+                    </div>
+                    <hr> 
+                </div>
+            </div>
+            
+            <admin-new-game-component ></admin-new-game-component>
 
-            <admin-new-game-component></admin-new-game-component>
-
-            <button route="/" class="button is-primary">Home</button>
         `;
     }
+    
+
 }
+
+
+
 
 customElements.define('admin-console-page', AdminConsolePage);
