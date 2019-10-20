460d7fe1c64b07222e4445106c888929c6de90d6
Edoardo Bortolozzo <edoardobortolozzo@gmail.com>
2019-10-20 17:45:10 +0200

    Aggiunto radar e collisioni

--- src/p5/admin-console.p5.js
+++ src/p5/admin-console.p5.js
@@ -14,20 +14,15 @@ export class AdminConsoleP5 {
         p.preload = () => {
             img = p.loadImage('./applicativi/mapset08.jpg');
         }
-        //TODO: qui va la logica del motore di gioco
+        
         p.setup = () => {
             p.createCanvas(1600, 1000);
-            p.background(img);
-            //p.background(255, 200, 220);
-            
+            p.background(img);            
             p.frameRate(1);
         }
+
         p.draw = () => {
-            if (this.gameIsPlaying) {
-                /*
-                p.ellipse(50, 50, 100, 100);
-                */
-                
+            if (this.gameIsPlaying) {                
                 //clock
                 this.gameData.info.gameTime++;
 
@@ -36,12 +31,14 @@ export class AdminConsoleP5 {
 
                 //aggiorno le info dei singoli team                
                 Object.keys(this.gameData.teams).forEach(i => {
-                    console.log('entra');
-                    Engine.updateTeams(this.gameData.teams[i]);
+                    Engine.updateTeams(this.gameData.teams[i], p);
                 });
                 
-                //disegno le navi sulla mappa
-                Engine.plotTeams(this.gameData.teams);
+                //disegno le navi sulla mappa dopo aver azzerato lo sfondo
+                p.background(img);
+                Object.keys(this.gameData.teams).forEach(i => {
+                    Engine.plotTeams(this.gameData.teams[i].outputs, p);
+                });
             }
         }
     }   
@@ -56,17 +53,17 @@ export class Engine {
         info.windForce = (info.windForce * 10 + (Math.floor((Math.random() * 2 * windMaxChange - windMaxChange) * 10) + 1)) / 10;
         if (info.windForce < 0) { info.windForce = 0 };
         if (info.windForce > windMaxSpeed) { info.windForce = windMaxSpeed }
-        console.log(info.windForce);
+        console.log('wind force = ' + info.windForce);
 
         let windMaxAngle = 10; 
 
         info.windDirection = info.windDirection + Math.floor(Math.random() * 2 * windMaxAngle - windMaxAngle);
         if (info.windDirection < 0) { info.windDirection += 360 }
         if (info.windDirection > 360) { info.windDirection += -360 }
-        console.log(info.windDirection);
+        console.log('wind angle = ' + info.windDirection);
     }
 
-    static updateTeams(team) {
+    static updateTeams(team, p) {
         //se una nave non viene usata la salto
         if (!team.outputs.isUsed) {return;}
 
@@ -74,13 +71,77 @@ export class Engine {
         this.updatePosition(team.outputs);
 
         //aggiorno velocita'
-        this.updateSpeed(team.outputs, team.inputs);
+        this.updateSpeed(team.inputs, team.outputs);
 
         //aggiorno carburante
         this.updateFuel(team.outputs);
 
-        //TODO: radar e collisioni
+        //radar e collisioni
+        this.updateRadar(team.outputs);
+        try {
+            this.checkCollisions(team.outputs, p);
+        } catch (e) {
+            //collisione avvenuta
+            team.outputs.speed = 0;
+            team.inputs.acceleration = 0;
+        }
+
         //TODO: controllo isUsed
+    }
+    
+    static checkCollisions(data, p) {
+        //coordinate da controllare
+        let posX = data.positionX + 10 * Math.cos(data.direction * Math.PI / 180);
+        let posY = data.positionY + 10 * Math.sin(data.direction * Math.PI / 180);
+
+        //controllo punto
+        let x = this.checkPoint(posX, posY, p);
+        //blocco la nave in caso di collisione
+        if (x != 1) {
+            data.radar.sate = x;
+        } else {
+            data.positionX -= (10) * Math.cos((data.direction - 90) * Math.PI / 180);
+            data.positionY -= (10) * Math.sin((data.direction - 90) * Math.PI / 180);
+            throw "Collided!";
+        }
+    }
+
+    static updateRadar(data, p) {
+        let radarDistance = 40; //raggio del radar
+        let radarGap = 10;      //distanza tra i singoli punti del radar
+
+        Object.keys(data.radar.frontStates).forEach(i => {
+            //calcolo la direzione in cui controllare il punto
+            //poi mi servira' in rad
+            let radarDirection = (data.direction + (radarGap * (i - 3)) * Math.PI / 180);
+            //aggiorno lo stato del radar
+            let posX = data.positionX + radarDistance * Math.cos(radarDirection);
+            let posY = data.positionY + radarDistance * Math.cos(radarDirection);
+            data.radar.frontStates[i] = this.checkPoint(posX, posY, p);
+        });
+    }
+
+    static checkPoint(posX, posY, p) {
+        //gestione colori per stato
+        let colorTollerance = 2; //tolleranza lettura pixel colore
+        let col1 = 0; // [0,0,0,255]; // nero terra
+        let col2 = 64; //[255,0,0,255]; // usi futuri
+        let col3 = 128; // obiettivo finale
+        let col4 = 192; //[0, 255, 0, 255]; // obbiettivi intermedi
+        let col5 = 255; //bianco mare
+
+        //ritorna aray rgba
+        let pointColor = p.get(posX, posY);
+        // traduce in scala di grigio;
+        let greyScaleColor = (Math.floor(pointColor[0] + pointColor[1] + pointColor[2]) / 3);
+        // controllo colore campionato (con tolleranza colorTollerance) colori vicini
+        let state = 0;
+        if ((greyScaleColor > (col1 - colorTollerance) && greyScaleColor < (col1 + colorTollerance))) { state = 1 };
+        if ((greyScaleColor > (col2 - colorTollerance) && greyScaleColor < (col2 + colorTollerance))) { state = 2 };
+        if ((greyScaleColor > (col3 - colorTollerance) && greyScaleColor < (col3 + colorTollerance))) { state = 3 };
+        if ((greyScaleColor > (col4 - colorTollerance) && greyScaleColor < (col4 + colorTollerance))) { state = 4 };
+        //ritorno lo stato del punto
+        return state;
     }
 
     static updatePosition(data) {
@@ -92,43 +153,43 @@ export class Engine {
         data.positionY += (data.speed * moltiplicatoreVelocita) * Math.sin((data.direction - 90) * Math.PI / 180);
 
         //tengo conto del vento (FIXME: futura scelta)
-        if (false) {
+        if (true) {
             data.positionX += (windForce * moltiplicatoreVelocita) * Math.cos((windDirection - 90) * Math.PI / 180);
             data.positionY += (windForce * moltiplicatoreVelocita) * Math.sin((windDirection - 90) * Math.PI / 180);
         }
     }
 
-    static updateSpeed(out, in) {
+    static updateSpeed(inputs, outputs) {
         // aggiorno le velocita'
-                    // la velocita raggiunge graduatamente l-acc con step di tre alla volta
-                    let x = out.speed;
-                    if (x < in.acceleration) {
-                        switch (in.acceleration - x) {
-                            case 1:
-                                x++;
-                                break;
-                            case 2:
-                                x += 2;
-                                break;
-                            default:
-                                x += 3;
-                        }
-                    }
-                    if (x > in.acceleration) {
-                        switch (x - in.acceleration) {
-                            case 1:
-                                x--;
-                                break;
-                            case 2:
-                                x -= 2;
-                                break;
-                            default:
-                                x -= 3;
-                        }
-                    }
-                    if (x < -5) { x = -5 }
-                    if (x > 20) { x = 20 }
-                    out.speed = x;
+        // la velocita raggiunge graduatamente l-acc con step di tre alla volta
+        let x = outputs.speed;
+        if (x < inputs.acceleration) {
+            switch (inputs.acceleration - x) {
+                case 1:
+                    x++;
+                    break;
+                case 2:
+                    x += 2;
+                    break;
+                default:
+                    x += 3;
+            }
+        }
+        if (x > inputs.acceleration) {
+            switch (x - inputs.acceleration) {
+                case 1:
+                    x--;
+                    break;
+                case 2:
+                    x -= 2;
+                    break;
+                default:
+                    x -= 3;
+            }
+        }
+        if (x < -5) { x = -5 }
+        if (x > 20) { x = 20 }
+        outputs.speed = x;
     }
 
     static updateFuel(data) {
@@ -150,7 +211,15 @@ export class Engine {
         }
     }
 
-    static plotTeams() {
-        //TODO: plotta navi
+    static plotTeams(data, p) {
+        //plotta navi
+        //salto se la nave non e' utilizzata
+        if (!data.isUsed) {return;}
+        //TODO: mod lunghezze in base alla direzione
+        let length = 10;
+        let width = 10;
+        p.ellipse(data.positionX, data.positionY, length, width);
     }
-} 
+}
+
+
